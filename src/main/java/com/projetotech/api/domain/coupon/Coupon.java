6f0c22493e741042c153d90b718cc100adea3952
victorsz08 <joaovictorsal777@gmail.com>
2026-08-20 19:24:52 -0300

    feat: first commint and creating entities

---
+++ src/main/java/com/projetotech/api/domain/coupon/Coupon.java
@@ -0,0 +1,40 @@
+package com.projetotech.api.domain.coupon;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.projetotech.api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "coupon")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+
+    private Integer discount;
+
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/com/projetotech/api/domain/event/Event.java
@@ -0,0 +1,39 @@
+package com.projetotech.api.domain.event;
+
+import java.util.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+
+    private String description;
+
+    private String imageUrl;
+
+    private String eventUrl;
+
+    private Boolean remote;
+
+    private Date date;
+}
